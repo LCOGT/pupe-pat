db1984be8e26eed031b2b95505378b806b12f735
Joey Chatelain <jchate6@gmail.com>
2024-03-11 16:37:50 -0700

    check for 1m as well as sbig

--- pupepat/plot.py
+++ pupepat/plot.py
@@ -45,8 +45,8 @@ def plot_best_fit_ellipse(output_filename, data_cutout, best_fit_model, header):
                 'Outer: {x: 0.3f}, {y: 0.3f}'.format(x=best_fit_model.x0_outer, y=best_fit_model.y0_outer),
                 color='white')
 
-    # If using an sbig camera, calculate the M1 offset, otherwise give offset in pixels
-    if 'kb' in header['INSTRUME'].lower():
+    # If using an sbig camera on a 1m, calculate the M1 offset, otherwise give offset in pixels
+    if 'kb' in header['INSTRUME'].lower() and '1m0' in header['TELID'].lower():
         pyplot.text(data_cutout.shape[1] * 0.01, data_cutout.shape[0] * 0.88,
                     'M1 Offset (mm): {x: 0.3f}, {y: 0.3f}'.format(x=calculate_donut_offset(best_fit_model.x0_inner,
                                                                                            best_fit_model.x0_outer,
